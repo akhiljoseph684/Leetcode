ef4403a2960d02723a700d4ac07be06cb2081fcf
Akhil Joseph <akhiljoseph684@gmail.com>
2025-11-24 11:27:53 +0530

    Time: 100 ms (88.10%) | Memory: 73.4 MB (95.88%) - LeetSync

---
+++ 2783-nested-array-generator/nested-array-generator.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+var inorderTraversal = function*(arr) {
+    arr = arr.flat(Infinity)
+    for(let i = 0; i < arr.length; i++){
+        yield arr[i];
+    }
+}
+ const gen = inorderTraversal([1, [2, 3]]);
+ gen.next().value;
+ gen.next().value;
+ gen.next().value;
