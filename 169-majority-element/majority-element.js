1af2feb0cdb5e8efc9350b9977ce3123866fcf9d
Akhil Joseph <akhiljoseph684@gmail.com>
2025-11-30 09:40:42 +0530

    Time: 2272 ms (5.02%) | Memory: 57.3 MB (69.46%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let greatest = 0;
+    let num;
+    for(let i = 0; i < nums.length; i++){
+        let count = 0;
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] === nums[j])count++
+        }
+        if(count > greatest){
+            num = nums[i]
+            greatest = count
+        }
+    }
+    if(!greatest)num = nums[0]
+    return num
+};
