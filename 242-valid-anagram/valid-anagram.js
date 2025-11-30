5bcc324ee10a990d9df3f99d7f855382df6c769b
Akhil Joseph <akhiljoseph684@gmail.com>
2025-11-30 10:17:20 +0530

    Time: 31 ms (15.73%) | Memory: 59.3 MB (32.67%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length)return false
+    s = s.split('')
+    s.sort()
+    t = t.split('')
+    t.sort()
+    for(let i = 0; i < s.length; i++){
+        if(s[i] !== t[i])return false
+    }
+    return true
+};
