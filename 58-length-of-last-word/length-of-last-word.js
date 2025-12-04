404698509c2c6a9944061dc0a4383128f9d76636
Akhil Joseph <akhiljoseph684@gmail.com>
2025-12-04 13:29:32 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (77.02%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    s = s.split(' ')
+    for(let i = s.length - 1; i >= 0; i--){
+        if(s[i] !== '') return s[i].length
+    }
+    return 0;
+};
