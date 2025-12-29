2202752a23691d79ea4a540e4a1eb1b2b0cb83b4
Akhil Joseph <akhiljoseph684@gmail.com>
2025-12-29 09:33:49 +0530

    Time: 11 ms (23.63%) | Memory: 65.9 MB (16.99%) - LeetSync

---
+++ 326-power-of-three/power-of-three.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n === 1)return true
+    while(n !== 3){
+        if(n < 3 ){
+            return false
+        }
+        n /= 3
+    }
+    return true
+};
