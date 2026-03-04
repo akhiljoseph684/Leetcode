80a73cd2a12e3f4439ac173b2b2a5fe372021917
Akhil Joseph <akhiljoseph684@gmail.com>
2026-03-04 09:27:31 +0530

    Time: 2 ms (20.22%) | Memory: 57.8 MB (5.06%) - LeetSync

---
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    nums.sort((a, b) => a - b);
+    let count = 0;
+    for(let i = 0; i < nums.length; i++){
+      if(nums[i] < k){
+        count++;
+      }else{
+        return count;
+      }
+    }
+};
