9300ac146961966234e4e328e2964901bddbec34
Akhil Joseph <akhiljoseph684@gmail.com>
2026-04-14 16:56:46 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (60.84%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    let arr = [];
+    function traversal(root){
+        if(!root)return;
+        traversal(root.left);
+        arr.push(root.val)
+        traversal(root.right);
+    }
+    traversal(root)
+    return arr;
+};
